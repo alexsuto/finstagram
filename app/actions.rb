6d5dcf13a5bfcafe080e93bc41ee00a3c17c1123
Alex Suto <alexandrasuto@hotmail.com>
2017-05-31 04:03:52 +0000

    created erb tab, loops and DRY

--- app/actions.rb
+++ app/actions.rb
@@ -6,11 +6,12 @@ def humanized_time_ago(time_ago_in_minutes)
     end
 end
 
-
 get '/' do
-    post_shark = {
+    
+    
+    @post_shark = {
         username: "sharky_j",
-        avartar_url: "http://naserca.com/images/sharky_j.jpg",
+        avatar_url: "http://naserca.com/images/sharky_j.jpg",
         photo_url: "http://naserca.com/images/shark.jpg",
         humanized_time_ago: humanized_time_ago(15),
         like_count: 0,
@@ -21,9 +22,9 @@ get '/' do
         }]
     }
     
-    post_whale = {
+    @post_whale = {
         username: "kirk_whalum",
-        avartar_url: "http://naserca.com/images/kirk_whalum.jpg",
+        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
         photo_url: "http://naserca.com/images/whale.jpg",
         humanized_time_ago: humanized_time_ago(65),
         like_count: 0,
@@ -34,9 +35,9 @@ get '/' do
         }]
     }
     
-    post_marlin = {
+    @post_marlin = {
         username: "marlin_peppa",
-        avartar_url: "http://naserca.com/images/marlin_peppa.jpg",
+        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
         photo_url: "http://naserca.com/images/marlin.jpg",
         humanized_time_ago: humanized_time_ago(190),
         like_count: 0,
@@ -48,6 +49,8 @@ get '/' do
         
     }
     
-    [post_shark, post_whale, post_marlin].to_s
+    @posts = [@post_shark, @post_whale, @post_marlin]
+    
+    erb:index
     
 end
